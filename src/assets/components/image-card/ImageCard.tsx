9b1416966e17fac8d1504c24a1e5313b00adda62
Eduardo Oliveira <eduardoliveira2009@gmail.com>
2023-12-10 19:35:36 +0000

    Added set Project main image #8

--- src/assets/components/image-card/ImageCard.tsx
+++ src/assets/components/image-card/ImageCard.tsx
@@ -1,29 +1,62 @@
-import { ActionIcon, AspectRatio, Card, Group, Image, LoadingOverlay, Modal, rem, Text, useMantineTheme } from '@mantine/core';
-import { IconZoomScan } from '@tabler/icons-react';
+import { ActionIcon, AspectRatio, Card, Group, Image, LoadingOverlay, Modal, rem, Text, useMantineTheme, Menu } from '@mantine/core';
+import { IconZoomScan, IconHeart } from '@tabler/icons-react';
 import classes from './ImageCard.module.css';
 import { useToggle } from "@mantine/hooks";
 import { DropDownMenu } from "../parts/drop-down-menu/DropDownMenu.tsx";
 import { AssetCardProps } from '../AssetCardProps.ts';
 import { SettingsContext } from '@/core/utils/settingsContext.ts';
 import { useCallback, useContext, useState } from 'react';
+import useAxios from 'axios-hooks';
+import { notifications } from '@mantine/notifications';
 
 export function ImageCard({ projectUuid, asset, selected, onSelectChange, onDelete }: AssetCardProps) {
     const { local_backend } = useContext(SettingsContext);
+    const [{ }, callSetMainImage] = useAxios(
+        {
+            url: `${local_backend}/projects/${projectUuid}/image`,
+            method: 'POST'
+        },
+        { manual: true }
+    );
     const [loading, setLoading] = useState(false);
     const toggleLoadingCallback = useCallback(() => {
         setLoading((l) => {
             return !l
         })
     }, [loading])
+    const setMainImage = useCallback(() => {
+        callSetMainImage({
+            data: {
+                uuid: projectUuid,
+                default_image_path: asset.sha1
+            }
+        })
+            .then(({ data }) => {
+                console.log(data);
+                notifications.show({
+                    title: 'Great Success!',
+                    message: 'Project main image updated!',
+                    color: 'indigo',
+                })
+            })
+            .catch(({ message }) => {
+                console.log(message)
+                notifications.show({
+                    title: 'Ops... Error updating project main image!',
+                    message,
+                    color: 'red',
+                    autoClose: false
+                })
+            });
+    }, [projectUuid]);
     const [value, toggle] = useToggle([false, true]);
     const theme = useMantineTheme();
 
-    const size = rem('280px');
     return (<>
         <Modal opened={value} onClose={() => toggle()}>
             <Image src={`${local_backend}/projects/${projectUuid}/assets/${asset.sha1}`} />
         </Modal>
-        <Card withBorder padding="lg" radius="md" className={classes.card} style={{ minWidth: size, width: size, borderColor: selected ? 'red' : '' }}>
+        <Card withBorder padding="lg" radius="md" className={classes.card} style={{ borderColor: selected ? 'red' : '' }}>
             <Card.Section mb="sm" onClick={() => toggle()}>
                 <AspectRatio ratio={16 / 9}>
                     <Image src={`${local_backend}/projects/${projectUuid}/assets/${asset.sha1}`} />
@@ -33,7 +66,7 @@ export function ImageCard({ projectUuid, asset, selected, onSelectChange, onDele
             <Text fw={700} className={classes.title} mt="xs" onClick={() => onSelectChange(true)}>
                 {asset.name}
             </Text>
-            
+
             <LoadingOverlay visible={loading} zIndex={1000} overlayProps={{ blur: 2 }} />
             <Card.Section className={classes.footer}>
                 <Group justify="flex-end">
@@ -52,6 +85,9 @@ export function ImageCard({ projectUuid, asset, selected, onSelectChange, onDele
                             downloadURL={`${local_backend}/projects/${projectUuid}/assets/${asset?.sha1}?download=true'`}
                             onDelete={() => onDelete(projectUuid, asset.sha1)}
                             toggleLoad={toggleLoadingCallback}>
+                            <Menu.Item onClick={setMainImage} leftSection={<IconHeart style={{ width: rem(14), height: rem(14) }} />}>
+                                Set as main image
+                            </Menu.Item>
                         </DropDownMenu>
                     </Group>
                 </Group>
