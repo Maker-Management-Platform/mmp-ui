ac05f2963e8d67f18fc67b734e22f4cd96a2d045
Eduardo Oliveira <eduardoliveira2009@gmail.com>
2023-12-21 11:58:23 +0000

    PoC prusa slicer integration. Not working as they filter the domain printables.com #28

--- src/assets/components/model/model-card/ModelCard.tsx
+++ src/assets/components/model/model-card/ModelCard.tsx
@@ -1,25 +1,27 @@
-import {AspectRatio, Card, Group, Image, LoadingOverlay, rem, Text} from '@mantine/core';
+import { ActionIcon, AspectRatio, Card, Group, Image, LoadingOverlay, rem, Text } from '@mantine/core';
 import classes from './ModelCard.module.css';
-import {SelectBtn} from "@/assets/components/parts/select-btn/SelectBtn.tsx";
+import { SelectBtn } from "@/assets/components/parts/select-btn/SelectBtn.tsx";
 import { DropDownMenu } from '../../parts/drop-down-menu/DropDownMenu';
-import { Icon3dRotate } from '@tabler/icons-react';
+import { Icon3dRotate, IconLayersSubtract } from '@tabler/icons-react';
 import { AssetCardProps } from '../../AssetCardProps';
 import { SettingsContext } from '@/core/utils/settingsContext';
 import { useCallback, useContext, useState } from 'react';
 
-export function ModelCard({projectUuid, asset, selected, onSelectChange, view3d, onView3dChange, onDelete}: AssetCardProps) {
-    const {local_backend} = useContext(SettingsContext);
+export function ModelCard({ projectUuid, asset, selected, onSelectChange, view3d, onView3dChange, onDelete }: AssetCardProps) {
+    const { local_backend } = useContext(SettingsContext);
     const [loading, setLoading] = useState(false);
     const toggleLoadingCallback = useCallback(() => {
-        setLoading((l)=>{
+        setLoading((l) => {
             return !l
         })
-    },[loading])
+    }, [loading])
     const size = rem('280px');
+
+    const sendToSlicerURL = `prusaslicer://open?file=${encodeURI(`${local_backend}/projects/${projectUuid}/assets/${asset?.sha1}`)}`;
     return (
-        <Card withBorder padding="lg" radius="md" className={classes.card} style={{ minWidth: size, width: size, borderColor: selected?'red':''}} >
-            <Card.Section mb="sm" onClick={()=>onSelectChange(true)}>
-                <AspectRatio ratio={16/9}>
+        <Card withBorder padding="lg" radius="md" className={classes.card} style={{ minWidth: size, width: size, borderColor: selected ? 'red' : '' }} >
+            <Card.Section mb="sm" onClick={() => onSelectChange(true)}>
+                <AspectRatio ratio={16 / 9}>
 
                     <Image
                         src={`${local_backend}/projects/${projectUuid}/assets/${asset?.model?.image_sha1}`}
@@ -28,7 +30,7 @@ export function ModelCard({projectUuid, asset, selected, onSelectChange, view3d,
                 </AspectRatio>
             </Card.Section>
 
-            <Text fw={700} className={classes.title} mt="xs" onClick={()=>onSelectChange(true)}>
+            <Text fw={700} className={classes.title} mt="xs" onClick={() => onSelectChange(true)}>
                 {asset.name}
             </Text>
 
@@ -36,15 +38,22 @@ export function ModelCard({projectUuid, asset, selected, onSelectChange, view3d,
             <Card.Section className={classes.footer}>
                 <Group justify="flex-end">
                     <Group gap={0}>
-                        {onView3dChange && view3d!==undefined && <SelectBtn selected={view3d} onChange={onView3dChange} icon={<Icon3dRotate/>}/>}
+                        <ActionIcon variant="subtle" color="gray" component='a' href={sendToSlicerURL}>
+                            <IconLayersSubtract
+                                style={{ width: rem(20), height: rem(20) }}
+                                color={'yellow'}
+                                stroke={1.5}
+                            />
+                        </ActionIcon>
+                        {onView3dChange && view3d !== undefined && <SelectBtn selected={view3d} onChange={onView3dChange} icon={<Icon3dRotate />} />}
                         <DropDownMenu
                             projectUuid={projectUuid}
                             sha1={asset.sha1}
-                                openDetails={()=>onSelectChange(true)}
-                                downloadURL={`${local_backend}/projects/${projectUuid}/assets/${asset?.sha1}?download=true'`}
-                                onDelete={() => onDelete(projectUuid, asset.sha1)}
-                                toggleLoad={toggleLoadingCallback}>
-                            </DropDownMenu>
+                            openDetails={() => onSelectChange(true)}
+                            downloadURL={`${local_backend}/projects/${projectUuid}/assets/${asset?.sha1}?download=true'`}
+                            onDelete={() => onDelete(projectUuid, asset.sha1)}
+                            toggleLoad={toggleLoadingCallback}>
+                        </DropDownMenu>
                     </Group>
                 </Group>
             </Card.Section>
