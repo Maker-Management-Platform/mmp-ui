4e8a0fa821f4621771122cf541ea1afafef63256
Eduardo Oliveira <eduardoliveira2009@gmail.com>
2024-01-14 12:05:44 +0000

    Improved printer edit page layout

--- src/printers/components/edit-printer-page/EditPrinterPage.tsx
+++ src/printers/components/edit-printer-page/EditPrinterPage.tsx
@@ -4,6 +4,8 @@ import { useContext } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import { PrinterForm } from "../parts/printer-form/PrinterForm";
 import { Printer } from "@/printers/entities/Printer";
+import { Header } from "@/core/header/Header";
+import { Container } from "@mantine/core";
 
 export function EditPrinterPage() {
     const navigate = useNavigate();
@@ -14,8 +16,11 @@ export function EditPrinterPage() {
         `${local_backend}/printers/${id}`
     );
     return (<>
-        <PrinterForm printer={data} onPrinterChange={function (p: any): void {
-            navigate("/printers")
-        }} />
+        <Header title={data?.name} imagePath={'https://images.unsplash.com/photo-1611117775350-ac3950990985?q=80&w=2000&h=400&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'} />
+        <Container mt='md'>
+            <PrinterForm printer={data} onPrinterChange={function (p: any): void {
+                navigate("/printers")
+            }} />
+        </Container>
     </>)
 }
