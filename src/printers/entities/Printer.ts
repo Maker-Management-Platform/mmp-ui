3ac16353d2d4b653931bbb5f81ff16813123a4cd
Nuno Gama Freire <nunardas@gmail.com>
2024-03-02 18:13:03 +0000

    Add octoprint image

--- src/printers/entities/Printer.ts
+++ src/printers/entities/Printer.ts
@@ -9,4 +9,4 @@ export interface Printer {
     apiKey?: string
 }
 
-export const printerTypes = new Map<string, any>([["klipper", { type: 'klipper', name: 'Klipper', logo: 'images/klipper-logo.png' }], ["octoPrint", { type: 'octoPrint', name: 'op', logo: 'images/klipper-logo.png' }]])
+export const printerTypes = new Map<string, any>([["klipper", { type: 'klipper', name: 'Klipper', logo: 'images/klipper-logo.png' }], ["octoPrint", { type: 'octoPrint', name: 'op', logo: 'images/octoprint-logo.png' }]])
